0da57ae2938b2a7922b5285d9e85fa9327d76384
pblim <archacki85@gmail.com>
2020-04-05 20:51:57 +0200

    Fix

--- main.py
+++ main.py
@@ -33,34 +33,6 @@ epic_list = {
     'Blinding Fire Barakiel': '2'
 }
 
-"""
-['Domb Death Cabrio', 'Alive', '-', ''],
-['Under Queenant Dron', 'Dead', 'WINDOW', '2020-04-05 16:56:00'],
-['Jeruna Queen', 'Dead', '-1 day, 23:41:09', '2020-04-05 17:09:00'],
-['Blinding Fire Barakiel', 'Dead', '0:37:09', '2020-04-05 18:05:00'],
-['Geyser Guardian Hestia', 'Dead', '1:16:09', '2020-04-05 18:44:00'],
-['Cherub Garacsia', 'Dead', '3:01:09', '2020-04-05 20:29:00'],
-['Shax The Death Lord', 'Dead', '19:49:09', '2020-04-06 13:17:00'],
-['Hallate The Death Lord', 'Dead', '20:15:09', '2020-04-06 13:43:00'],
-['Golkonda Longhorn', 'Dead', '20:16:09', '2020-04-06 13:44:00'],
-['Kernon', 'Dead', '20:23:09', '2020-04-06 13:51:00'],
-['Uruka', 'Dead', '1 day, 0:34:09', '2020-04-06 18:02:00'],
-['King Tiger Karuta', 'Dead', '1 day, 4:06:09', '2020-04-06 21:34:00'],
-['Antharas1', 'Dead', '1 day, 6:06:09', '2020-04-06 23:34:00'],
-['Queen Ant', 'Dead', '1 day, 13:10:09', '2020-04-07 06:38:00'],
-['Zaken', 'Dead', '1 day, 14:35:09', '2020-04-07 08:03:00'],
-['Ocean Flame Ashakiel', 'Dead', '1 day, 23:45:09', '2020-04-07 17:13:00'],
-['Demonic Agent Falston', 'Dead', '1 day, 23:54:09', '2020-04-07 17:22:00'],
-['Cronoss Summons Mumu', 'Dead', '2 days, 0:53:09', '2020-04-07 18:21:00'],
-['Ereve Deathman', 'Dead', '2 days, 1:37:09', '2020-04-07 19:05:00'],
-['Amber', 'Dead', '2 days, 1:53:09', '2020-04-07 19:21:00'],
-['Archon Susceptor', 'Dead', '2 days, 1:55:09', '2020-04-07 19:23:00'],
-['Varka Commnder Mos', 'Dead', '2 days, 1:55:09', '2020-04-07 19:23:00'],
-['Ketra Commander Tayr', 'Dead', '2 days, 2:05:09', '2020-04-07 19:33:00'],
-['Papurrion Pingolpin', 'Dead', '2 days, 2:33:09', '2020-04-07 20:01:00'],
-['Orfen', 'Dead', '2 days, 4:07:09', '2020-04-07 21:35:00'],
-['Valakas', 'Dead', '2 days, 8:01:09', '2020-04-08 01:29:00'],
-['Baium', 'Dead', '3 days, 1:26:09', '2020-04-08 18:54:00']]"""
 
 def get_boses():
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0'}
@@ -134,31 +106,4 @@ def internal_server_error(e):
 if __name__ == "__main__":
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(debug=True)
-
-'''Core  72 hours +/- 1 hour (retail level no custom drops)
-Orfen  72 hours +/- 1 hour (retail level no custom drops)
-Zaken  84 hours +/- 1 hour (retail level no custom drops)
-Queen Ant  84 hours +/- 1 hour 
-Queen Ant DRONE 24 hours +/- 1 hour :fire: NEW RAID BOSS 
-Baium  120 hours +/- 1 hour
-Antharas  168 hours +/- 1 hour
-Valakas   216 hours +/- 1 hour
-
-King Tiger Karuta  -  4 days +/-1 hour respawn time (Drop full Platinum - location PI cave) OPPOSITE SIDE OF SAILREN
-:fire: Uruka - 4 days +/-1 hour respawn time (Drop full Platinum - location PI beach) :boom: NEW RETAIL LOCATION
-:fire: Ocean Flame Ashakiel   - 4 days +/-1 hour respawn (Drop full Platinum - location near PI Sailren) NEAR SAILREN
-
-Amber    - 3 days +/- 1 hour respawn time (Various drops including Platinum parts) RETAIL LOCATION
-Varka Commnder Mos    - 3 days +/- 1 hour respawn time (Various drops including Platinum parts) RETAIL LOCATION
-Archon Susceptor    - 3 days +/- 1 hour respawn time (Various drops including Platinum parts) RETAIL LOCATION
-Death Lord SHAX    - 3 days +/- 1 hour respawn time (Various drops including Platinum parts) RETAIL LOCATION
-Papurrion Pingolpin   - 3 days +/- 1 hour respawn time (Various drops including Platinum parts)  RETAIL LOCATION
-Anima27/03/2020
-Cherub Galaxia    - 3 days +/- 1 hour respawn time (Various drops including Platinum parts) RETAIL LOCATION
-Cronoss Summons Mumu    - 3 days +/- 1 hour respawn time (Various drops including Platinum parts) RETAIL LOCATION
-Demonic Agent Falston    - 3 days +/- 1 hour respawn time (Various drops including Platinum parts) RETAIL LOCATION
-Ereve Deathman    - 3 days +/- 1 hour respawn time (Various drops including Platinum parts) RETAIL LOCATION
-Guardian Hestia    - 3 days +/- 1 hour respawn time (Various drops including Platinum parts) RETAIL LOCATION
-Queen Zyrnna    - 3 days +/- 1 hour respawn time (Various drops including Platinum parts) RETAIL LOCATION
-Ketra Commander Tayr    - 3 days +/- 1 hour respawn time (Various drops including Platinum parts) RETAIL LOCATION'''
+    app.run(debug=False)
